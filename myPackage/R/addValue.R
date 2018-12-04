e1f5d29fc7c64ada01da75c48abb024d8d8cde89
fpohl <felix@pohlsweb.de>
2018-12-04 12:07:48 +0100

    package test

---
+++ myPackage/R/addValue.R
@@ -0,0 +1,12 @@
+#' @description A function that adds values.
+#' @title a function that adds values
+#' 
+#' @param x one value
+#' @param y another value
+#' 
+#' @examples 
+
+addValue <- function(x,y){
+  return(x+y)
+}
+
